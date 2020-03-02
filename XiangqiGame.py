b8a860f595e1f1c0f4625af016fcd035cc07ea16
Jillian Crowley <JillianCrowley01@gmail.com>
2020-03-02 11:25:53 -0500

    2nd commit

--- XiangqiGame.py
+++ XiangqiGame.py
@@ -5,7 +5,7 @@
 
 class XiangqiGame:
     """
-    Represents a XiangqiGame with id_code and title data members.
+    Represents a XiangqiGame with _the_board data member.
     """
 
     def __init__(self):
@@ -15,7 +15,7 @@ class XiangqiGame:
         with columns labeled a-i and rows labeled 1-10, with row 1 being the Red side and row 10 the Black side
         """
         # _the_board
-        self._the_board = [["r", "a1", "r", "b1", "r", "c1", "r", "d1", "r", "e1", "r", "f1", "r", "g1", "h1", "i1"],
+        self._the_board_test = [["r", "a1", "r", "b1", "r", "c1", "r", "d1", "r", "e1", "r", "f1", "r", "g1", "h1", "i1"],
                            ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2", "i2"],
                            ["a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3", "i3"],
                            ["a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4", "i4"],
@@ -26,10 +26,23 @@ class XiangqiGame:
                            ["a9", "b9", "c9", "d9", "e9", "f9", "g9", "h9", "i9"],
                            ["a10", "b10", "c10", "d10", "e10", "f10", "g10", "h10", "i10"]]
 
+        self._the_board = \
+            [["red_chariot_1", "red_horse_1", "red_elephant_1", "red_advisor_1", "red_general", "red_advisor_2", "red_elephant_2", "red_horse_2"", "red_chariot_2"],
+             ["", "", "", "", "", "", "", "", ""],
+             ["", "", "", "", "", "", "", "", ""],
+             ["", "", "", "", "", "", "", "", ""],
+             ["", "", "", "", "", "", "", "", ""],
+             ["", "", "", "", "", "", "", "", ""],
+             ["", "", "", "", "", "", "", "", ""],
+             ["", "", "", "", "", "", "", "", ""],
+             ["", "", "", "", "", "", "", "", ""],
+             ["black_chariot_1", "black_horse_1", "black_elephant_1", "black_advisor_1", "black_general", "black_advisor_2", "black_elephant_2", "black_horse_2", "black_chariot_2"]]
+
         self._game_state = "UNFINISHED"
 
     def get_the_board(self):
         """Returns the board."""
+        column_labels = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
         return self._the_board
 
     def get_game_state(self):
