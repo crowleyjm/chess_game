a8d603b937ac9c7b31298ec931ba37fcbdd7e8ba
Jillian Crowley <JillianCrowley01@gmail.com>
2020-03-05 01:01:53 -0500

    8th commit

--- XiangqiGame.py
+++ XiangqiGame.py
@@ -1,4 +1,4 @@
-# Author: Jillian Crowley
+# Author: Jillian Crowle
 # Date: 03/04/2020
 # Description: From Wikipedia:
 # The Xiangqi game (Chinese chess) represents a battle between two 16-piece armies, with
