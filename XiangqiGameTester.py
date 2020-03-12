0d076da173e8f64a7c14902e8d6752a9de5d1445
Jillian Crowley <JillianCrowley01@gmail.com>
2020-03-11 23:18:28 -0400

    37th commit

--- XiangqiGameTester.py
+++ XiangqiGameTester.py
@@ -148,7 +148,7 @@ class TestXiangqiGame(unittest.TestCase, XiangqiGame, General, Advisor, Elephant
         final_result = (result_1, result_2, result_3, result_4, result_5, result_6)
         self.assertEqual(final_result, (True, True, True, True, False, False))
 
-    def test_red_wins(self):
+    def test_red_wins_1(self):
         game = XiangqiGame()
         result_1 = game.make_move('h3', 'e3')
         result_2 = game.make_move('h8', 'g8')
@@ -180,6 +180,58 @@ class TestXiangqiGame(unittest.TestCase, XiangqiGame, General, Advisor, Elephant
                          (True, True, True, True, True, True, True, True, True, True,
                           True, True, True, True, True, True, True, True, True, True,
                           True, True, "RED_WON"))
+
+    def test_black_wins_1(self):
+        game = XiangqiGame()
+        result_1 = game.make_move('h3', 'f3')
+        result_2 = game.make_move('b8', 'e8')
+        result_3 = game.make_move('f3', 'f6')
+        result_4 = game.make_move('h10', 'g8')
+        result_5 = game.make_move('h1', 'g3')
+        result_6 = game.make_move('i10', 'h10')
+        result_7 = game.make_move('c4', 'c5')
+        result_8 = game.make_move('a10', 'a9')
+        result_9 = game.make_move('c5', 'c6')
+        result_10 = game.make_move('a9', 'f9')
+        result_11 = game.make_move('c6', 'c7')
+        result_12 = game.make_move('f9', 'f6')
+        result_13 = game.make_move('c7', 'd7')
+        result_14 = game.make_move('f6', 'd6')
+        result_15 = game.make_move('d7', 'e7')
+        result_16 = game.make_move('g8', 'e7')
+        result_17 = game.make_move('i1', 'h1')
+        result_18 = game.make_move('d6', 'd3')
+        result_19 = game.make_move('c1', 'e3')
+        result_20 = game.make_move('d3', 'b3')
+        result_21 = game.make_move('a1', 'a3')
+        result_22 = game.make_move('b3', 'b1')
+        result_23 = game.make_move('a3', 'd3')
+        result_24 = game.make_move('h8', 'h2')
+        result_25 = game.make_move('h1', 'h2')
+        result_26 = game.make_move('h10', 'h2')
+        result_27 = game.make_move('g3', 'e2')
+        result_28 = game.make_move('b10', 'c8')
+        result_29 = game.make_move('d3', 'c3')
+        result_30 = game.make_move('e7', 'd5')
+        result_31 = game.make_move('c3', 'c8')
+        result_32 = game.make_move('d5', 'f4')
+        result_33 = game.make_move('c8', 'e8')
+        result_34 = game.is_in_check("black")
+        result_35 = game.make_move('g10', 'e8')
+        result_36 = game.make_move('e4', 'e5')
+        result_37 = game.make_move('f4', 'd3')
+        result_38 = game.get_game_state()
+
+        final_result = (result_1, result_2, result_3, result_4, result_5, result_6, result_7, result_8, result_9,
+                        result_10, result_11, result_12, result_13, result_14, result_15, result_16, result_17,
+                        result_18, result_19, result_20, result_21, result_22, result_23, result_24, result_25,
+                        result_26, result_27, result_28, result_29, result_30, result_31, result_32, result_33,
+                        result_34, result_35, result_36, result_37, result_38)
+        self.assertEqual(final_result,
+                         (True, True, True, True, True, True, True, True, True, True,
+                          True, True, True, True, True, True, True, True, True, True,
+                          True, True, True, True, True, True, True, True, True, True,
+                          True, True, True, True, True, True, True, "BLACK_WON"))
 
 
 if __name__ == '__main__':
